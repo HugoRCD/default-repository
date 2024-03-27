8b6072278cd448114990a854ab4babc424886092
HugoRCD <hugo.richard@epitech.eu>
2024-03-27 18:31:43 +0100

    refactor: issue template

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import { expect, it } from 'vitest'
 import { main } from './index'
 
-it('prints \'Hello, world!\'', () => {
+it('prints \'Hello, world!\'', (): void => {
   expect(main()).toMatch('Hello, world!')
 })
