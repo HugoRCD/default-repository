a5105eab6a9d664ffc4d6a966db5b956e593e382
HugoRCD <hugo.richard@epitech.eu>
2024-03-28 13:48:48 +0100

    feat: more test and init coverage report

---
+++ test/maths.test.ts
@@ -0,0 +1,19 @@
+import { expect, it, describe } from 'vitest'
+
+describe('maths', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toStrictEqual(3)
+  })
+
+  it('subtracts 2 - 1 to equal 1', () => {
+    expect(2 - 1).toStrictEqual(1)
+  })
+
+  it('multiplies 2 * 3 to equal 6', () => {
+    expect(2 * 3).toStrictEqual(6)
+  })
+
+  it('divides 6 / 3 to equal 2', () => {
+    expect(6 / 3).toStrictEqual(2)
+  })
+})
